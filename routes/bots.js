fa75dd0f9b4dff8d3f5561333d933bab5b1432fc
samwalker <samwalker505@gmail.com>
2016-04-16 18:01:22 +0800

    init :

---
+++ routes/bots.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// Index route
+router.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+
+router.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+module.exports = router;
