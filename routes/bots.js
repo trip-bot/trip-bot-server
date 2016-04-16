cff709ed71e1c40e3ddea91c094d450da5f75cfc
samwalker <samwalker505@gmail.com>
2016-04-16 19:28:53 +0800

    facebook token

--- routes/bots.js
+++ routes/bots.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res) {
 
 // for Facebook verification
 
-router.get('/webhook/', function (req, res) {
+router.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
