222714e5be0d35e3b24c7dfca0ed4a6a869ce5d7
samwalker <samwalker505@gmail.com>
2016-04-16 18:20:03 +0800

    renew app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var bots = require('./routes/bots')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/bots', bots)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
