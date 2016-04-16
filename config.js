41207c6d213281c2d2973e0b8498d8a14d20fdf2
samwalker <samwalker505@gmail.com>
2016-04-16 22:05:30 +0800

    added config

---
+++ config.js
@@ -0,0 +1,20 @@
+
+module.exports = function(){
+  console.log(`using env: ${process.env.NODE_ENV}`);
+  switch(process.env.NODE_ENV){
+      case 'development':
+          return {
+            "host":"http://localhost:3000"
+          };
+
+      case 'production':
+          return {
+            "host":"https://travel-bot-fb.herokuapp.com"
+          };
+
+      default:
+          return {
+            "host":"http://localhost:3000"
+          };
+  }
+};
