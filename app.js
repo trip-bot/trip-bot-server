030db7d7e6422fca5be76bfa7f5957bd8a4db559
samwalker <samwalker505@gmail.com>
2016-04-16 23:34:40 +0800

    added master code

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
-var FB = require('fb');
+const passport = require("passport");
+const Strategy = require("passport-facebook").Strategy;
+const FB = require("fb");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var bots = require('./routes/bots');
-var facebook = require('./routes/facebook');
+const routes = require("./routes/index");
+const users = require("./routes/users");
+const bots = require("./routes/bots");
+const facebook = require("./routes/facebook");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 passport.use(new Strategy({
-    clientID: '1688582574742376',
-    clientSecret: 'cb9f3bdff27218a9ff421c0d41cb3c35',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    clientID: "1688582574742376",
+    clientSecret: "cb9f3bdff27218a9ff421c0d41cb3c35",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {
+  (accessToken, refreshToken, profile, cb) => {
     // In this example, the user's Facebook profile is supplied as the user
     // record.  In a production-quality application, the Facebook profile should
     // be associated with a user record in the application's database, which
@@ -38,41 +38,44 @@ passport.use(new Strategy({
     return cb(null, profile);
   }));
 
-passport.serializeUser(function(user, cb) {
+passport.serializeUser((user, cb) => {
     cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser((obj, cb) => {
     cb(null, obj);
 });
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/bots', bots);
-app.use('/facebook', facebook);
+app.use("/", routes);
+app.use("/users", users);
+app.use("/bots", bots);
+app.use("/facebook", facebook);
 
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
+app.get("/auth/facebook",
+  passport.authenticate("facebook"));
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login/facebook' }),
-  function(req, res) {
-    res.redirect('/facebook/me');
+app.get("/auth/facebook",
+  passport.authenticate("facebook", { scope: [ "email", "user_posts" ] }));
+
+app.get("/auth/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login/facebook" }),
+  (req, res) => {
+    res.redirect("/facebook/me");
   });
-// app.use('/facebook', facebook);
+// app.use("/facebook", facebook);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -81,10 +84,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use((err, req, res) => {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -93,9 +96,9 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
