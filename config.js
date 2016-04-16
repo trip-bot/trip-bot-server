ff7a57efff8ac3d9d755325d24fcecada675b2a8
samwalker <samwalker505@gmail.com>
2016-04-17 02:36:48 +0800

    personality checking work

--- config.js
+++ config.js
@@ -2,19 +2,17 @@
 module.exports = function(){
   console.log(`using env: ${process.env.NODE_ENV}`);
   switch(process.env.NODE_ENV){
-      case 'development':
-          return {
-            "host":"http://localhost:3000",
-          };
-
       case 'production':
           return {
             "host":"https://travel-bot-fb.herokuapp.com",
-
+            "piAccount": "7057441d-8aab-4521-a258-81d05497fb74",
+            "piPassword": "jZRLxd6KHtM0"
           };
 
       default:
           return {
+            "piAccount": "7057441d-8aab-4521-a258-81d05497fb74",
+            "piPassword": "jZRLxd6KHtM0",
             "host":"http://localhost:3000"
           };
   }
