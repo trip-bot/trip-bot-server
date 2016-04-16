34f2a0901a6b6fc9d1fb73bfa6ff580fdb4483b5
Billy Kwok <billykwok@Billys-MacBook-Pro.local>
2016-04-16 21:14:45 +0800

    Rename package.json app name, add eslint dependencies and implement message echo

--- routes/bots.js
+++ routes/bots.js
@@ -3,17 +3,48 @@ var router = express.Router();
 
 /* GET home page. */
 // Index route
-router.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
+router.get("/", function (req, res) {
+  res.send('Hello world, I am a chat bot')
+});
 
 // for Facebook verification
+router.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === "my_voice_is_my_password_verify_me") {
+      res.send(req.query["hub.challenge"])
+  }
+  res.send("Error, wrong token")
+});
 
-router.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+router.post("/webhook/", function (req, res) {
+  req.body.entry[0].messagings.map(event => {
+    var sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var text = event.message.text;
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
-    res.send('Error, wrong token')
-})
+  });
+  res.sendStatus(200);
+});
+
+const token = "CAAXZCwcSKm2gBAGSWc5QIclBVzStH4ICBWhBmv5Sb0ONlao9ufC0nZBzGzcH7qdYBI3mT2mFhi9zRLJipGlw9VffZBbQrkzyTpiBkvjLfIMAZBVogD38lZAgeXWZBJsSE8gYkW7jLLInegQql9ki2vW8QAjqwP6YOUoVfQZCWlA38bTCrD0Ezj8aIXtgczLglWzZCLDYyrnLmAZDZD";
+
+function sendTextMessage(sender, text) {
+  messageData = { text: text };
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: { access_token: token },
+    method: "POST",
+    json: {
+      recipient: { id: sender },
+      message: messageData,
+    }
+  }, (error, response, body) => {
+    if (error) {
+      console.log("Error sending message: ", error);
+    } else if (response.body.error) {
+      console.log("Error: ", response.body.error);
+    }
+  });
+}
 
 module.exports = router;
