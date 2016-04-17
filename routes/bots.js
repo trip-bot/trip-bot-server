40122d30829aa19c07b66e28c5144af9a28c95e1
Billy Kwok <billykwok@Billys-MacBook-Pro.local>
2016-04-17 08:45:45 +0800

    fix array.find polyfill

--- routes/bots.js
+++ routes/bots.js
@@ -1,9 +1,31 @@
-require("array.prototype.find");
 const request = require("request");
 const express = require("express");
 const messenger = require("../others/messenger");
 const Wit = require("node-wit").Wit;
 const router = express.Router();
+
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError("Array.prototype.find called on null or undefined");
+    }
+    if (typeof predicate !== "function") {
+      throw new TypeError("predicate must be a function");
+    }
+    const list = Object(this);
+    const length = list.length >>> 0;
+    const thisArg = arguments[1];
+    let value;
+
+    for (let i = 0; i < length; ++i) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
 
 const token = "CAAMoDMGAW7cBAKGjBYbW2vUKq6uE515ZBFuJ5TvWILBbDyl7sC6mT4vIEmHIZCDOjhNYABqPa5O3nI9fiDxcDL40K0sXIWHf0ZBsBZBDHlpF4dL5MvZCSFy1mY4sGqoAWKzKsxDpldOH87cgtafrYgBUvLbCzHIyGtaZCWvZC53yaqxXS3sIhXOwdk08mzuHgLw7u9CjS53uAZDZD";
 const sessions = new Map();
