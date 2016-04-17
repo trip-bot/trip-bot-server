47eb31e3b7729a712b2cc75e66f6e2e33621ee25
Billy Kwok <billykwok@Billys-MacBook-Pro.local>
2016-04-17 12:07:44 +0800

    add 2nd and 3rd h-view

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const locationService = require("../services/locationService");
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  locationService.getLocation((locations, other) => {
-    res.send(locations);
-  });
 });
 
 module.exports = router;
